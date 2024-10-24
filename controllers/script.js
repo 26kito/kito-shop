669b158699f55dfd899af9b55964fe6a2a9d7c46
kito <surya.aw26@gmail.com>
2024-10-24 09:52:39 +0700

    add logo and price list page

--- controllers/script.js
+++ controllers/script.js
@@ -1,4 +1,4 @@
-const fetchContent = (data) => {
+const fetchAllContent = (data) => {
     let template = ''
 
     data.forEach((d) => {
@@ -17,25 +17,61 @@ const fetchContent = (data) => {
     return template
 }
 
+const fetchContent = (data) => {
+    let template = ''
+    let counter = 0
+    let limit = 6
+
+    data.forEach((d) => {
+        if (counter < limit) {
+            template += `
+                <div class="col-lg-2 col-md-3 col-sm-4 col-6 mb-4">
+                    <div class="card text-center">
+                        <img src="${d.imageUrl}" class="card-img-top" alt="${d.name}">
+                        <div class="card-body">
+                            <p class="card-title">${d.name}</p>
+                        </div>
+                    </div>
+                </div>
+            `
+
+            counter++
+        }
+    })
+
+    return template
+}
+
 const categories = {
     games: gamesList,
     voucher: voucherList,
     entertainment: entertainmentList
 };
 
-// Page init content
+// Page init main content
 $('#content-wrapper').html(fetchContent(categories['games']));
 
 // Fetch the content when any category is clicked
 $('.category-menu div').on('click', (event) => {
     const category = $(event.currentTarget).data('category');
     $('#content-wrapper').html(fetchContent(categories[category]));
-
+    
     // Remove 'active' class from all and set to clicked category
     $('.category-menu div').removeClass('active');
     $(event.currentTarget).addClass('active');
 });
 
+// Page init fetch all content
+$('#content-wrapper-all-products').html(fetchAllContent(categories['games']));
+
+$('.category-menu-all-products div').on('click', (event) => {
+    const category = $(event.currentTarget).data('category');
+    $('#content-wrapper-all-products').html(fetchAllContent(categories[category]));
+    
+    // Remove 'active' class from all and set to clicked category
+    $('.category-menu-all-products div').removeClass('active');
+    $(event.currentTarget).addClass('active');
+});
 
 // toastr notification function
 const notifMessage = [
@@ -65,5 +101,5 @@ setInterval(() => {
     } else {
         currentIndex = 0 // Reset to the first notification
     }
-}, 1000 * 30)
+}, 1000 * 10)
 // toastr notification function
--- models/games-list.js
+++ models/games-list.js
@@ -23,4 +23,44 @@ const gamesList = [
         name: "Honkai Impact 3rd",
         imageUrl: "https://i.pinimg.com/736x/51/25/37/5125376df13eb5570308e2aa4eae454f.jpg"
     },
+    {
+        name: "Honkai Star Rail",
+        imageUrl: "https://stardb.gg/images/icons/star-rail-icon.webp"
+    },
+    {
+        name: "Free Fire",
+        imageUrl: "https://play-lh.googleusercontent.com/jft5dckkhc_7hjsZBgEorUA6C3jUxkjYhEIR6R-WFMaeKvvd8DVtDXQj1O6Dit7bj9s=w240-h480-rw"
+    },
+    {
+        name: "Zenless Zone Zero",
+        imageUrl: "https://preview.redd.it/zenless-zone-zero-icon-v0-9ss6qz6tuwx81.png?auto=webp&s=a1ae22c3c24df4db08c9c540313d59f60776957e"
+    },
+    {
+        name: "Clash of Clans",
+        imageUrl: "https://play-lh.googleusercontent.com/LByrur1mTmPeNr0ljI-uAUcct1rzmTve5Esau1SwoAzjBXQUby6uHIfHbF9TAT51mgHm"
+    },
+    {
+        name: "Onmyoji Arena",
+        imageUrl: "https://play-lh.googleusercontent.com/AgZr-zAYXMjE7VtBVe-RFRff0z8gaM7mPzEgjiVaI2sq7oYPjfaMMnjsThzM4y4ViA=w240-h480-rw"
+    },
+    {
+        name: "Call of Duty Mobile",
+        imageUrl: "https://iconpusher.com/_next/image?url=https%3A%2F%2Fimg.iconpusher.com%2Fcom.garena.game.codm%2F20210801.png&w=640&q=75"
+    },
+    {
+        name: "Nikke: Goddess of Victory",
+        imageUrl: "https://cdn2.steamgriddb.com/icon/29ec343546213d078be89400d9bf76d4.ico"
+    },
+    {
+        name: "Brawl Stars",
+        imageUrl: "https://play-lh.googleusercontent.com/_vDz2dbDEPcE0vYKED3Qi5bVo_C2E-w0ceVdrDJ9Ey52ExYgIRv_TFTb7AF-oS5sRw"
+    },
+    {
+        name: "Hay Day",
+        imageUrl: "https://play-lh.googleusercontent.com/Za8u0ChNK-3mZ3rnLcBirRBTzLYTdqaaAn63Ha9qKZiDp_bF_5u7OGfvWb1dg88WttKw"
+    },
+    {
+        name: "Lokapala",
+        imageUrl: "https://play-lh.googleusercontent.com/tvJfgBT_hLnAxmIjAZyumu9ApRM2AlsrYVzLG3z4fYPzlSb0SNrlR4y2ZvWBUUTvyw=w240-h480-rw"
+    },
 ]
